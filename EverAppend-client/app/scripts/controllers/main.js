38348903d4168addc8a47140f5e1dd0e5ebe036e
Harold Treen <haroldtreen@gmail.com>
2014-07-31 00:41:46 -0400

    Basic authentication working with Oauth.io

--- EverAppend-client/app/scripts/controllers/main.js
+++ EverAppend-client/app/scripts/controllers/main.js
@@ -8,10 +8,18 @@
  * Controller of the everAppendClientApp
  */
 angular.module('everAppendClientApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', ['$scope', '$resource', function ($scope, $resource) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+    $scope.authEvernote = function(){
+    	OAuth.initialize('');
+
+    	OAuth.popup('evernote_sandbox').done(function(result) {
+		    console.log(result);
+			});
+    };
+  }]);
